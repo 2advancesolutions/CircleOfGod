34bbc4caee474ea71b14e549d7c9ea31a425bda4
reginald <reginald@ntersol.com>
2021-08-21 12:12:21 -0500

    init setup added Ionic PWA / Web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
   title = 'test';
 }
