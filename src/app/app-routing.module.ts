65ff0ba62a1b34865c9970a9f879c6938a664e9f
reginald <reginald@ntersol.com>
2021-08-22 13:04:28 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './componets/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'profile',
+  },
+  { path: 'profile', component: UserProfileComponent },
+  {
+    path: '**',
+    component: UserProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const ArrayOfComponents = [UserProfileComponent];
