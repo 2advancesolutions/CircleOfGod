d475d3965514c41a7401b0ba429b99db2f8a4acb
reginald <reginald@ntersol.com>
2021-08-29 22:53:02 -0500

    code  clean up

--- platforms/ios/www/9072.304cdc9fc81ce1a4bac9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktest=self.webpackChunktest||[]).push([[9072],{9072:(h,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var t=s(3150),c=s(7585),_=s(1269);const n=class{constructor(o){(0,t.r)(this,o)}render(){const o=(0,c.b)(this);return(0,t.h)(t.H,{class:(0,_.c)(this.color,{[o]:!0})},(0,t.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
