c660a52e0b0629d49dd4b5fe4d3e3a3cd8d3029a
reginald <reginald@ntersol.com>
2021-09-23 11:00:34 -0500

    added base key superbase req low level key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   supabaseUrl: "https://uopwsawvoaqbggzcfxip.supabase.co",
-  supbaseKey: ""
+  supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjI5ODU1MjA0LCJleHAiOjE5NDU0MzEyMDR9.xVqUyvCqDpXTpDrcW_zI8ZfAJGhvyArPbK4hXCVhGag"
 };
