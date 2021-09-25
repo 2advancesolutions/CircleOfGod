e9bfffc8b82d0e8cf901cdc95b951088e7125613
reginald <reginald@ntersol.com>
2021-09-25 11:33:19 -0500

    updated auth supabase TODO add interceptor to handel auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   supabaseUrl: "https://uopwsawvoaqbggzcfxip.supabase.co",
-  supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjI5ODU1MjA0LCJleHAiOjE5NDU0MzEyMDR9.xVqUyvCqDpXTpDrcW_zI8ZfAJGhvyArPbK4hXCVhGag"
+  supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjI5ODU1MjA0LCJleHAiOjE5NDU0MzEyMDR9.xVqUyvCqDpXTpDrcW_zI8ZfAJGhvyArPbK4hXCVhGag",
+  accessToken: ""
 };
