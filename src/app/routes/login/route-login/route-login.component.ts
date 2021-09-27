c486bcbf81aac988695719194a17a29474248bcf
reginald <reginald@ntersol.com>
2021-09-27 15:37:07 -0500

    added signup login now able to sign in after account is created

--- src/app/routes/login/route-login/route-login.component.ts
+++ src/app/routes/login/route-login/route-login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GlobalConfig } from 'src/app/global/project.config';
+import { HttpHelperService } from 'src/app/services/http-helper/http-helper.service';
 import { SupabaseService } from 'src/app/services/supabase.service';
+import { AuthService } from 'src/app/store/auth/state/auth.service';
+
 
 @Component({
   selector: 'app-route-login',
@@ -8,22 +14,57 @@ import { SupabaseService } from 'src/app/services/supabase.service';
 })
 export class RouteLoginComponent implements OnInit {
 
-  loading = false;
+  public loading = false;
+  public registerForm!: FormGroup;
+  public submitted = false;
+  private emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
 
-  constructor(private readonly supabase: SupabaseService) { }
+  constructor(private readonly supabase: SupabaseService, 
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private http: HttpHelperService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        userName: [
+          '',
+          [Validators.required, Validators.pattern(this.emailPattern)],
+        ],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      },
+      {}
+    );
   }
 
-  async handleLogin(input: string) {
-    try {
-      this.loading = true;
-      await this.supabase.signIn(input);
-      alert('Check your email for the login link!');
-    } catch (error: any) {
-      alert(error.error_description || error.message)
-    } finally {
-      this.loading = false;
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  public onSubmit(): void {
+    if (this.registerForm.invalid) {
+      this.submitted = true;
+      return;
+    } else {
+      const { userName, password } = this.f;
+      const obj = {
+        email: userName.value,
+        password: password.value,
+      };
+      const url = GlobalConfig.superbase.api.auth.signInEmail.url;
+      this.http
+        .post(url, obj)
+        .toPromise()
+        .then(
+          (data: any) => {
+            localStorage.setItem('session', JSON.stringify(data));
+         this.router.navigateByUrl('/main');
+          },
+          (error) => {
+             alert('Invaild User Name and Password');
+          }
+        );
     }
   }
 
